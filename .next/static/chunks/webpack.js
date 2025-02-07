b3de721534c4c44ee30b82123a111ccb74782814
indahtheresia <indahtheresiatheh29@gmail.com>
2025-02-07 13:20:11 +0800

    Use programmatic (imperative) navigation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d43d3edba7cd023c"; }
+/******/ 		__webpack_require__.h = function() { return "8ce2f3701d87af52"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
