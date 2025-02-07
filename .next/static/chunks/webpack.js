3331d9c4d3cfa9b9b4719d1be87d30a1c2c96bd8
indahtheresia <indahtheresiatheh29@gmail.com>
2025-02-07 16:59:34 +0800

    Explore server-side rendering (SSR) with 'getServerSideProps'

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1a65be231544b09a"; }
+/******/ 		__webpack_require__.h = function() { return "3e197c0b95cf4c88"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
