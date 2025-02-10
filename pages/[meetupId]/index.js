59d1612527ea456291b815cda19439deaaa1e19e
indahtheresia <indahtheresiatheh29@gmail.com>
2025-02-10 10:53:00 +0800

    Prepare paths with 'getStaticPaths' & work with fallback pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,13 +11,31 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ]
+  }
+}
+
 export async function getStaticProps(context) {
   //fetch data for a single meetup
   
   const meetupId = context.params.meetupId;
 
   console.log(meetupId);
-  
+
   return {
     props: {
       meetupData: {
